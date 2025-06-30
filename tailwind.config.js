b40df9ce579af80477d018468a3d1065840c3f84
Shinee <batsvkhshinebayr@gmail.com>
2025-06-30 13:48:37 +0800

    feat: Update primary color configuration in Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#3aeb34",
+        primary: { default: "#3aeb34" },
       },
       animation: {
         ripple: "ripple 0.6s linear",
